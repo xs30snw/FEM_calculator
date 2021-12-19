bd4488630e4336e322a0eaf305377cdebca8ecf6
xs30snw <e4n22s@gmail.com>
2021-12-19 21:21:29 +0500

    Webpack initial setup

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const SRC = path.resolve(__dirname, 'src');
+const PUBLIC = path.resolve(__dirname, 'public');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+module.exports = {
+    entry: {
+        index: [
+            './src/App.tsx',
+            './src/styles/style.css'
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: SRC,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.tsx$/i,                
+                include: SRC,
+                use: {
+                    loader: 'esbuild-loader',
+                    options: {
+                        loader: 'tsx',
+                        target: 'es2015'
+                    }
+                }
+            },
+        ],
+    },
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new ESBuildMinifyPlugin({
+                target: 'es2015',
+                css: false
+            })
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: false,
+            minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: PUBLIC,
+        clean: true,
+    },  
+    devtool: "source-map",
+};
